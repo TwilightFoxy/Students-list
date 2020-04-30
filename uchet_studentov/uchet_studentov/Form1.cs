2a07b47feb30aa49722d11343ad1fca7200a5c49
Levon <5novel5@mail.ru>
2020-04-30 19:46:30 +0300

    Вывод студентов на экран
    Вывод студентов работает нормально, но лучше будет если я буду сортировать их по группам

--- uchet_studentov/uchet_studentov/Form1.cs
+++ uchet_studentov/uchet_studentov/Form1.cs
@@ -23,5 +23,23 @@ namespace uchet_studentov
             New_St student = new New_St();
             student.ShowDialog();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string writePath = @"C:\Users\User\Documents\Students\List.txt";//Позже буду использовать локальную папку в которой находится .exe
+            Directory.CreateDirectory("C:\\Users\\User\\Documents\\Students");
+
+            richTextBox1.Clear();
+            using (FileStream fstream = new FileStream(writePath, FileMode.OpenOrCreate))
+            {
+                byte[] output = new byte[4];
+                fstream.Seek(0, SeekOrigin.Begin);
+                output = new byte[fstream.Length];
+                fstream.Read(output, 0, output.Length);
+                // декодируем байты в строку
+                string textFromFile = Encoding.Default.GetString(output);
+                richTextBox1.Text = textFromFile;
+            }
+        }
     }
 }
