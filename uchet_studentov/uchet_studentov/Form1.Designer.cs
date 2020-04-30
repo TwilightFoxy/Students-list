1bbc2264d9dd179ff8ffd02c9e7773f3117503d9
Levon <5novel5@mail.ru>
2020-04-30 19:46:30 +0300

    Вывод студентов на экран
    Вывод студентов работает нормально, но лучше будет если я буду сортировать их по группам

--- uchet_studentov/uchet_studentov/Form1.Designer.cs
+++ uchet_studentov/uchet_studentov/Form1.Designer.cs
@@ -29,24 +29,47 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(12, 16);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(252, 147);
+            this.button1.Size = new System.Drawing.Size(190, 30);
             this.button1.TabIndex = 0;
             this.button1.Text = "Добавить студента";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(208, 16);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(580, 422);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(12, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(190, 30);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Обновить данные";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -58,6 +81,8 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
