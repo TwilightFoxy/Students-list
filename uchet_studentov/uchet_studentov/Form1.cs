79db7dfac06170b3066b69b71b7c8bf061b37625
Levon <5novel5@mail.ru>
2020-05-02 12:59:17 +0300

    Обработка студентов
    Начинаю работу над модулем обработки студентов.

--- uchet_studentov/uchet_studentov/Form1.cs
+++ uchet_studentov/uchet_studentov/Form1.cs
@@ -13,10 +13,12 @@ namespace uchet_studentov
 {
     public partial class Form1 : Form
     {
+        string writePath = @"C:\Users\User\Documents\Students\List.txt";//Позже буду использовать локальную папку в которой находится .exe
+
         public Form1()
         {
             InitializeComponent();
-            string writePath = @"C:\Users\User\Documents\Students\List.txt";//Позже буду использовать локальную папку в которой находится .exe
+           // string writePath = @"C:\Users\User\Documents\Students\List.txt";//Позже буду использовать локальную папку в которой находится .exe
             Directory.CreateDirectory("C:\\Users\\User\\Documents\\Students");
 
             richTextBox1.Clear();
@@ -54,6 +56,25 @@ namespace uchet_studentov
                 // декодируем байты в строку
                 string textFromFile = Encoding.Default.GetString(output);
                 richTextBox1.Text = textFromFile;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Clear();
+            using (FileStream fstream = new FileStream(writePath, FileMode.OpenOrCreate))
+            {
+                byte[] output = new byte[4];
+                fstream.Seek(0, SeekOrigin.Begin);
+                output = new byte[fstream.Length];
+                fstream.Read(output, 0, output.Length);
+                // декодируем байты в строку
+
+                string textFromFile = Encoding.Default.GetString(output);
+                string[] mystring = textFromFile.Split('\n');
+                //Разделил файл на строки, теперь разделим строки на слова.
+                int N = mystring.Length-1;
+                richTextBox2.Text = "" + N;
             }
         }
     }
